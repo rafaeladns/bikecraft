6060f43e3348202f6430df9b70f9ba5516975322
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2022-02-24 19:05:56 -0300

    Parte de perguntas frequentes abrindo e fechando ok

--- js/script.js
+++ js/script.js
@@ -19,8 +19,24 @@ function ativarProduto(parametro){
   if(elemento){
     elemento.checked = true;
   }
-  
-  console.log(elemento);
 }
 parametros.forEach(ativarProduto);
+
+//Expandir área das perguntas frequentes//
+const perguntas = document.querySelectorAll('.perguntas button');
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+
+  const controls = pergunta.getAttribute('aria-controls');
+
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa'); //verificar se tem ativa//
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas)
 
